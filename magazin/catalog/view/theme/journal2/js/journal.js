0712ef53e7762d6452e2d6aaa0adc19acde62654
Szentgyörgyi Csabi <csabi@dm-studio.ro>
2016-03-11 09:49:36 +0200

    sort sorted (puns aren't funny, but neither is debugging javascript)

--- magazin/catalog/view/theme/journal2/js/journal.js
+++ magazin/catalog/view/theme/journal2/js/journal.js
@@ -1650,7 +1650,7 @@ $(document).ready(function(){
 
     $('.sort ul li a').on('click', function(){
 
-        var selected_sort = $(this).attr('id');
+        var selected_sort = $(this).attr('data-sort');
 
         $('.sort ul li a').each(function() {
             $(this).removeClass('selected');
@@ -1661,7 +1661,6 @@ $(document).ready(function(){
         $(".sort > select > option").each(function() {
             $(this).removeAttr('selected');
         });
-        console.log('soort cli '+selected_sort);
         $('.sort > select > option[value$="'+selected_sort+'"]').prop('selected', true);
         //$('.sort select option[value$="' + sort + "&" + order +'"]').attr('selected', 'selected');
     });
