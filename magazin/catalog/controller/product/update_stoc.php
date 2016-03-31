8854a73ad2acd095bbb0eeb95be541d1031d940a
Szentgyörgyi Csabi <csabi@dm-studio.ro>
2016-03-31 16:17:13 +0300

    update stock modified

--- magazin/catalog/controller/product/update_stoc.php
+++ magazin/catalog/controller/product/update_stoc.php
@@ -5,22 +5,17 @@ class ControllerProductUpdateStoc extends Controller
 
     public function index()
     {
-        $script_started = time();
-        //die('alma');
-        $updating_stock_at_0 = 0;
-        if ( $updating_stock_at_0 == 0 )
-        {
-            // updated product quantity at 0 in oc_product table
-            $this->db->query( "UPDATE oc_product SET quantity = 0;" );
+        $tables = array('product', 'product_option_value', 'product_option_combination');
 
-            // updated product quantity at 0 in oc_product_option_value table
-            $this->db->query( "UPDATE oc_product_option_value SET quantity = 0;" );
-
-            // updated product quantity at 0 in oc_product_option_value table
-            $this->db->query( "UPDATE oc_product_option_combination SET stock = 0;" );
-
-            $updating_stock_at_0++;
+        foreach ($tables as $table) {
+            $show_create_table_query = $this->db->query("SHOW CREATE TABLE " . DB_PREFIX . $table)->row['Create Table'];
+            $this->db->query(str_replace(DB_PREFIX . $table,  DB_PREFIX . $table . "_new", $show_create_table_query));
+            $this->db->query("INSERT " . DB_PREFIX . $table . "_new SELECT * FROM " . DB_PREFIX . $table);
         }
+
+        $this->db->query("UPDATE " . DB_PREFIX . "product_new SET quantity = 0");
+        $this->db->query("UPDATE " . DB_PREFIX . "product_option_value_new SET quantity = 0");
+        $this->db->query("UPDATE " . DB_PREFIX . "product_option_combination_new SET stock = 0");
 
         $query = $this->db->query( "SELECT * FROM _AX_STOC" );
         if( $query->num_rows > 0 )
@@ -48,19 +43,19 @@ class ControllerProductUpdateStoc extends Controller
                 {
 //                    print "SELECT * FROM oc_product WHERE product_id='".$id."';<br>";
 //                    print "PRODUCT: UPDATE oc_product SET quantity='".$data['stoc']."' WHERE product_id='".$id."'; <br><br>";
-                    $this->db->query( "UPDATE oc_product SET quantity='".$data['stoc']."' WHERE product_id='".$id."';" );
+                    $this->db->query( "UPDATE oc_product_new SET quantity='".$data['stoc']."' WHERE product_id='".$id."';" );
                 }
                 else if( $type == 2 ) // option
                 {
 //                    print "SELECT * FROM oc_product_option_value WHERE product_option_value_id='".$id."';<br>";
 //                    print "PRODUCT_OPTION: UPDATE oc_product_option_value SET quantity = '".$data['stoc']."' WHERE product_option_value_id  = '".$id."';<br><br>";
-                    $this->db->query( "UPDATE oc_product_option_value SET quantity = '".$data['stoc']."' WHERE product_option_value_id  = '".$id."';" );
+                    $this->db->query( "UPDATE oc_product_option_value_new SET quantity = '".$data['stoc']."' WHERE product_option_value_id  = '".$id."';" );
                 }
                 else if( $type == 3 ) // option combination
                 {
 //                    print "SELECT * FROM oc_product_option_combination WHERE product_option_combination_id='".$id."';<br>";
 //                    print "PRODUCT_OPTION_COMBINATION: UPDATE oc_product_option_combination SET quantity = '".$data['stoc']."' WHERE product_option_combination_id  = '".$id."';<br><br>";
-                    $this->db->query( "UPDATE oc_product_option_combination SET stock = '".$data['stoc']."' WHERE product_option_combination_id  = '".$id."';" );// quantity = '".$data['stoc']."'
+                    $this->db->query( "UPDATE oc_product_option_combination_new SET stock = '".$data['stoc']."' WHERE product_option_combination_id  = '".$id."';" );// quantity = '".$data['stoc']."'
                 }
             }
             else
@@ -68,13 +63,10 @@ class ControllerProductUpdateStoc extends Controller
                 //print "Codul concatenat ".$data['concatenat']." nu figureaza in tabeleul ax_code!<br>";
             }
         }
-        $script_ended = time();
-        $handle = fopen(DIR_LOGS . 'ax_import.txt', 'a');
-        fwrite($handle, '[' . date(DATE_ATOM, $script_started) . '] script started' . PHP_EOL);
-        fwrite($handle, print_r($_SERVER, true) . PHP_EOL);
-        fwrite($handle, '[' . date(DATE_ATOM, $script_ended) . '] script ended, executed in: ' . $this->secondsToTime($script_ended - $script_started) . PHP_EOL);
-        fwrite($handle, PHP_EOL . str_repeat(str_repeat('=', 60) . PHP_EOL, 2) . PHP_EOL);
-        fclose($handle);
+        foreach ($tables as $table) {
+            $this->db->query("DROP TABLE IF EXISTS " . DB_PREFIX . $table);
+            $this->db->query("RENAME TABLE " . DB_PREFIX . $table . "_new TO " . DB_PREFIX . $table);
+        }
         print "OK";
         
     }
