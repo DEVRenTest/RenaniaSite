250fae6a73947f2679623921740afabedb66ab6f
istvanszekely <istvan@dm-studio.ro>
2016-09-13 14:19:15 +0300

    Language files (added missing strings in english)

---
+++ magazin/catalog/language/english/product/latest.php
@@ -0,0 +1,34 @@
+<?php
+$_['text_refine'] = 'Subcategories';
+$_['text_heading'] = 'New products | Renania | Leader in Safety Equipment';
+$_['text_breadcrumbs'] = 'NEW PRODUCTS';
+$_['text_product'] = 'products';
+$_['text_error'] = 'Category not found!';
+$_['text_empty'] = 'No products in this category.';
+$_['text_quantity'] = 'Quantity:';
+$_['text_manufacturer'] = 'Manufacturer:';
+$_['text_model'] = 'Model:';
+$_['text_points'] = 'Reward points:';
+$_['text_price'] = 'Price:';
+$_['text_tax'] = 'Without VAT:';
+$_['text_reviews'] = 'Based on %s reviews.';
+$_['text_compare'] = 'Compare products (%s)';
+$_['text_display'] = 'View as:';
+$_['text_list'] = 'List';
+$_['text_grid'] = 'Grid';
+$_['text_sort'] = 'Sort by:';
+$_['text_default'] = 'Default';
+$_['text_name_asc'] = 'Name (A - Z)';
+$_['text_name_desc'] = 'Name (Z - A)';
+$_['text_price_asc'] = 'Price (Low &gt; High)';
+$_['text_price_desc'] = 'Price (High &gt; Low)';
+$_['text_rating_asc'] = 'Rating (Lowest)';
+$_['text_rating_desc'] = 'Rating (Highest)';
+$_['text_model_asc'] = 'Model (A - Z)';
+$_['text_model_desc'] = 'Model (Z - A)';
+$_['text_date_added_asc'] = 'Date added ASC';
+$_['text_date_added_desc'] = 'Date added DESC';
+$_['text_limit'] = 'Products on page:';
+
+$_['button_sort_toggle']	= 'C<br />A<br />T<br />E<br />G<br />O<br />R<br />I<br />E<br />S';
+?>
---
+++ magazin/catalog/language/english/reclamation/reclamation.php
@@ -0,0 +1,47 @@
+<?php
+$_['heading_title'] = 'Reclamation';
+$_['text_reclamation'] = 'Reclamation';
+$_['text_send_reclamation'] = 'Send reclamation';
+$_['text_account'] = 'Account';
+$_['text_add_reclamation'] = 'Add reclamation';
+$_['text_mod_reclamation'] = 'Edit reclamation';
+$_['text_list_reclamation'] = 'LIST RECLAMATIONS';
+$_['entry_number'] = 'Reclamation number';
+$_['entry_subject'] = 'Subject';
+$_['entry_description'] = 'Description';
+$_['entry_documents'] = 'Documents:';
+$_['entry_status'] = 'Status';
+
+$_['error_subject'] = 'Subject required';
+$_['error_description'] = 'Description of the reclamation can not be empty!';
+$_['error_post_max_size']       = 'The file size is greater than 1% (see PHP setting \'post_max_size\')';
+$_['error_upload_max_filesize'] = 'The file size is greater than 1% (see PHP setting \'upload_max_filesize\')';
+$_['error_upload']              = 'Notification has been sent successfully, but does not meet the uploaded file! (In terms of format or size)';
+
+$_['text_send'] = 'Send';
+$_['text_add_another_one'] = 'Add another one';
+$_['text_add_success'] = 'Reclamation has been registered successfully!';
+$_['text_edit'] = 'Edit';
+$_['text_delete'] = 'Delete';
+$_['text_insert_date'] = 'Date';
+$_['text_empty'] = 'There is no reclamations!';
+$_['text_mod_success'] = 'Reclamation has been changed successfully!';
+$_['text_new'] = 'new';
+$_['text_in_progress'] = 'in progress';
+$_['text_resolved'] = 'resolved';
+$_['text_attached_documents'] = 'Attached document(s)';
+$_['text_are_you_sure'] = 'Are you sure?';
+$_['text_delete_success'] = 'Reclamation has been removed successfully!';
+$_['text_history'] = 'History';
+$_['text_reply'] = 'Reply';
+$_['text_cancel'] = 'Cancel';
+$_['text_notification_complaint_subject'] = 'Reclamation notification';
+$_['text_notification_return_product'] = 'Notification: No. return order ';
+$_['text_hello'] = 'Hello,';
+$_['text_notification_complaint_message'] = 'You received a reclamation! <br>To answer this you must login to the admin and access section Catalog/Complaints.';
+$_['text_allowed_extensions'] = 'Allowed extensions: doc, xls, xlsx, pdf, jpg, png.';
+$_['text_file'] = 'File(s) - <strong>';
+$_['text_does_not_meet'] = ' </strong> - does not meet!';
+$_['text_no_document'] = 'There is no document!';
+
+?>
