0a77be156e6e264fdd4f294965ca5182a84800a1
Norbi <norbi@nicsoft.eu>
2016-11-08 12:55:55 +0200

    Reviev system sql2 file  add title

--- magazin/oc_review2.sql
+++ magazin/oc_review2.sql
@@ -1,3 +1,4 @@
 /* Inainte sa copiati fisierele rulati sriptul aceasta. Daca aveti reviewuri in sistem si nu vreti sa stergeti rulati scriptul oc_review2*/
 ALTER TABLE oc_review ADD ip varchar(255) NOT NULL;
 ALTER TABLE oc_review ADD bought tinyint(1) NOT NULL; 
+ALTER TABLE oc_review ADD title varchar(255);
