324eef248c79ae3b5780983903d35fb145e8745d
Norbi <norbi@nicsoft.eu>
2016-11-10 15:16:08 +0200

    Review system psuh2

---
+++ magazin/admin/language/romana/catalog/review_ns.php
@@ -0,0 +1,46 @@
+<?php
+// Heading
+$_['heading_title']       = 'Lista comentariilor';
+$_['setting_title']       = 'Setări';
+
+// Text
+$_['text_success']      	= 'Succes: Ai modificat comentariile!';
+$_['emails_text']       	= 'Notificare prin email:';
+$_['emails_instruction']    = 'Introduceti emailurile separate cu virgula (exemplu: email1,email2)';
+$_['review_status']    		= 'Permite Opinii:';
+$_['review_status_instruction']    = 'Permite vizitatorilor sa scrie opinii despre produse';
+
+// Buttom
+$_['button_publica']    = 'Publică';
+// Column
+$_['column_title']    	= 'Title';
+$_['column_opinie']    	= 'Opinia cumpărătorului';
+$_['column_product']    = 'Produs';
+$_['column_author']     = 'Autor';
+$_['column_rating']     = 'Evaluare';
+$_['column_status']     = 'Publicat';
+$_['column_date_added'] = 'Data Adăugării';
+$_['column_bought']     = 'Achiziționat';
+$_['column_ip']     	= 'Ip';
+$_['column_action']     = 'Acțiune';
+
+
+// Entry
+$_['entry_product']     = 'Produs:<br/><span class="help">(Completare Automata)</span>';
+$_['entry_author']      = 'Autor:';
+$_['entry_rating']      = 'Evaluare:';
+$_['entry_status']      = 'Publicat:';
+$_['entry_text']        = 'Text:';
+$_['entry_good']        = 'Bun';
+$_['entry_bad']         = 'Rău';
+$_['entry_ip']          = 'Ip';
+$_['entry_bought']      = 'Achiziționat';
+
+// Error
+$_['error_permission']  = 'Atenție: Nu ai permisiunea să modifici comentariile!';
+$_['error_product']     = 'Produsul este necesar!';
+$_['error_author']      = 'Autorul trebuie să fie între 3 si 64 de caractere!';
+$_['error_title']      = 'Titlul trebuie să fie între 3 si 64 de caractere!';
+$_['error_text']        = 'Textul Opiniei trebuie sa fie cel putin de 1 caracter!';
+$_['error_rating']      = 'Evaluarea opiniei este necesară!';
+?>
